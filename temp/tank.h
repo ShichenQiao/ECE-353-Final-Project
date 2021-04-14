49d3f26b673ba4ca4335d3ef1bd2281bcdb4303d
ShichenQiao <71228724+ShichenQiao@users.noreply.github.com>
2021-04-13 22:17:39 -0500

    Add files via upload
    upload temp data file

---
+++ temp/tank.h
@@ -0,0 +1,15 @@
+/* Bitmap info for tank_front */
+extern const uint8_t tank_frontBitmaps;
+extern const uint8_t tank_frontWidthPixels;
+extern const uint8_t tank_frontHeightPixels;
+
+/* Bitmap info for tank_right */
+extern const uint8_t tank_rightBitmaps;
+extern const uint8_t tank_rightWidthPixels;
+extern const uint8_t tank_rightHeightPixels;
+
+/* Bitmap info for tank_left */
+extern const uint8_t tank_leftBitmaps;
+extern const uint8_t tank_leftWidthPixels;
+extern const uint8_t tank_leftHeightPixels;
+
