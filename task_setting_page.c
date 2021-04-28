fc23f8493c17acbe0bae5a96eea4fd5bb3bde31f
ShichenQiao <71228724+ShichenQiao@users.noreply.github.com>
2021-04-27 22:10:08 -0500

    Add files via upload
    added comment

--- task_setting_page.c
+++ task_setting_page.c
@@ -13,7 +13,9 @@ int game_duration_seconds = 120;        // Game duration in seconds, 60s ~ 300s 
 int number_of_colors = 11;       // Number of colors used in game (3 to 11, default 11)
 
 /******************************************************************************
- *
+ * Task managing the settings of BREAKER. The game duration, number of colors
+ * used in game, and background color of the game board will be configured
+ * in this order every time the setting page is entered from the home page.
  ******************************************************************************/
 void Task_Setting_Page(void *pvParameters)
 {
@@ -71,7 +73,9 @@ void Task_Setting_Page(void *pvParameters)
 }
 
 /******************************************************************************
- *
+ * Set the duration of a game. The possible values are 60s, 90s, ... 300s.
+ * Default duration is 120s. Press S1 will change to next level with wrap around.
+ * Press S2 will go to next setting.
  ******************************************************************************/
 void set_game_duration_seconds()
 {
@@ -182,7 +186,9 @@ void set_game_duration_seconds()
 }
 
 /******************************************************************************
- *
+ * Set the number of colors used in a game. The valid value is between 3 and 11.
+ * the default value is 11. Press S1 will change to next level with wrap around.
+ * Press S2 will go to next setting.
  ******************************************************************************/
 void set_number_of_colors()
 {
--- task_setting_page.h
+++ task_setting_page.h
@@ -16,19 +16,24 @@ extern int game_duration_seconds;        // Game duration in seconds.
 extern int number_of_colors;        // Number of colors used in game (3 to 11, default 11)
 
 /******************************************************************************
- *
+ * Task managing the settings of BREAKER. The game duration, number of colors
+ * used in game, and background color of the game board will be configured
+ * in this order every time the setting page is entered from the home page.
  ******************************************************************************/
 void Task_Setting_Page(void *pvParameters);
 
 /******************************************************************************
- *
+ * Set the duration of a game. The possible values are 60s, 90s, ... 300s.
+ * Default duration is 120s. Press S1 will change to next level with wrap around.
+ * Press S2 will go to next setting.
  ******************************************************************************/
 void set_game_duration_seconds();
 
 /******************************************************************************
- *
+ * Set the number of colors used in a game. The valid value is between 3 and 11.
+ * the default value is 11. Press S1 will change to next level with wrap around.
+ * Press S2 will go to next setting.
  ******************************************************************************/
 void set_number_of_colors();
-
 
 #endif /* __TASK_SETTING_PAGE_H__ */
